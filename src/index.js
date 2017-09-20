e0d356d764e27b9215b3a9881498346b20b13591
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-09-20 17:45:13 +0200

    feat: implement lazyloader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-const sample = 'intersection-lazyload';
+const defaultLoaderImplementation = element => {
+  // eslint-disable-next-line no-param-reassign
+  element.src = element.dataset.src;
+};
 
-export default sample;
+const onIntersection = loader => (entries, observer) => {
+  entries.forEach(entry => {
+    const { isIntersecting, target } = entry;
+
+    if (isIntersecting) {
+      loader(target);
+      observer.unobserve(target);
+    }
+  });
+};
+
+const lazyload = (
+  {
+    selector = '.--lazyload',
+    loader = defaultLoaderImplementation,
+    ...options
+  } = {},
+) => {
+  const observer = new IntersectionObserver(onIntersection(loader), options);
+
+  [].slice.call(document.querySelectorAll(selector)).forEach(element => {
+    observer.observe(element);
+  });
+};
+
+export default lazyload;
